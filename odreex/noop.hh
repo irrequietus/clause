ed272f80e7250229ccd063aea646cbb2746a648d
George Makrydakis <irrequietus@gmail.com>
2013-08-05 00:23:12 +0300

    Fixing header template blueprint layout in noop files.

--- odreex/noop.hh
+++ odreex/noop.hh
@@ -1,5 +1,25 @@
-#ifndef _ODREEX_HH_
-#define _ODREEX_HH_
+/**
+ * Copyright (C) 2013, George Makrydakis <irrequietus@gmail.com>
+ * 
+ * This file is part of odreex.
+ * odreex is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * odreex is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with odreex. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+#ifndef _ODREEX_NOOP_HH_
+#define _ODREEX_NOOP_HH_
+
 #include <cstdio>
 
 namespace odreex {
@@ -12,4 +32,4 @@ struct api {
 } /* noop */
 } /* odreex */
 
-#endif /* _ODREEX_HH_ */
+#endif /* _ODREEX_NOOP_HH_ */
