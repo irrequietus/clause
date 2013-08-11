fbc9c83fc644dfc0dbc3d6aa20832505bedab467
George Makrydakis <irrequietus@gmail.com>
2013-08-12 00:06:56 +0300

    Added tests makefile for ample, autotools housekeeping related fixes.

--- fheaders.mk
+++ fheaders.mk
@@ -1,5 +1,7 @@
 nobase_odreex_include_HEADERS = \
+	odreex/ample/oprt/fundamentals.hh \
 	odreex/ample/test/test.hh \
+	odreex/ample/base/basic_number.hh \
 	odreex/ample/test.hh \
 	odreex/noop.hh
 
