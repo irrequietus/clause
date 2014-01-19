3d8c294e754f69aeeb60e50d87f1f21a6cff58f3
George Makrydakis <irrequietus@gmail.com>
2014-01-19 18:57:39 +0200

    Tests for zip and zipwith for ppmpf sequences ranging 2 - 6.

--- tests/ppmpf/seq_zip.cc
+++ tests/ppmpf/seq_zip.cc
@@ -30,21 +30,58 @@ PPMPF_TEST( ppmpf_seq_zip
 ((i)(8))((j)(9)))
 
 PPMPF_TEST( ppmpf_seq_zip3
-          , "PPMPF_SEQ_ZIP (zip using 3 ppmpf sequences)"
+          , "PPMPF_SEQ_ZIP3 (zip using 3 ppmpf sequences)"
           , PPMPF_SEQ_ZIP3( (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
                           , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9)
                           , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j) )
           , ((a)(0)(a))((b)(1)(b))((c)(2)(c))((d)(3)(d))((e)(4)(e))((f)(5)(f))\
 ((g)(6)(g))((h)(7)(h))((i)(8)(i))((j)(9)(j)))
 
+PPMPF_TEST( ppmpf_seq_zip4
+          , "PPMPF_SEQ_ZIP4 (zip using 4 ppmpf sequences)"
+          , PPMPF_SEQ_ZIP4( (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
+                          , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9)
+                          , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
+                          , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9) )
+          , ((a)(0)(a)(0))((b)(1)(b)(1))((c)(2)(c)(2))((d)(3)(d)(3))\
+((e)(4)(e)(4))((f)(5)(f)(5))((g)(6)(g)(6))((h)(7)(h)(7))((i)(8)(i)(8))\
+((j)(9)(j)(9)))
+
+PPMPF_TEST( ppmpf_seq_zip5
+          , "PPMPF_SEQ_ZIP5 (zip using 5 ppmpf sequences)"
+          , PPMPF_SEQ_ZIP5( (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
+                          , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9)
+                          , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
+                          , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9)
+                          , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j) )
+          , ((a)(0)(a)(0)(a))((b)(1)(b)(1)(b))((c)(2)(c)(2)(c))\
+((d)(3)(d)(3)(d))((e)(4)(e)(4)(e))((f)(5)(f)(5)(f))((g)(6)(g)(6)(g))\
+((h)(7)(h)(7)(h))((i)(8)(i)(8)(i))((j)(9)(j)(9)(j)))
+
+PPMPF_TEST( ppmpf_seq_zip6
+          , "PPMPF_SEQ_ZIP6 (zip using 6 ppmpf sequences)"
+          , PPMPF_SEQ_ZIP6( (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
+                          , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9)
+                          , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
+                          , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9)
+                          , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
+                          , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9) )
+          , ((a)(0)(a)(0)(a)(0))((b)(1)(b)(1)(b)(1))((c)(2)(c)(2)(c)(2))\
+((d)(3)(d)(3)(d)(3))((e)(4)(e)(4)(e)(4))((f)(5)(f)(5)(f)(5))\
+((g)(6)(g)(6)(g)(6))((h)(7)(h)(7)(h)(7))((i)(8)(i)(8)(i)(8))\
+((j)(9)(j)(9)(j)(9)))
+
 PPMPF_TEST_BLOCK( ppmpf
                 , check_sequence_zip
                 , ( ppmpf_seq_zip
-                  , ppmpf_seq_zip3 )
+                  , ppmpf_seq_zip3
+                  , ppmpf_seq_zip4
+                  , ppmpf_seq_zip5
+                  , ppmpf_seq_zip6 )
                 , true )
 
 int main() {
     PPMPF_TEST_RUN( check_sequence_zip
-                  , "testing ppmpf PPMPF_SEQ_ZIP (2 - 3 sequences)" );
+                  , "testing ppmpf PPMPF_SEQ_ZIP (2 - 6 sequences)" );
     return {};
 }
