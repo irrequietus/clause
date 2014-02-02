994f77a3be6df2c446ce91cc1559668c4cbf96d8
George Makrydakis <irrequietus@gmail.com>
2014-02-03 00:14:39 +0200

    Final ppmpf sequence tests conversion to PPMPF_TST, #13.

--- tests/ppmpf/seq_at09.cc
+++ tests/ppmpf/seq_at09.cc
@@ -21,55 +21,65 @@
 #include <odreex/test.hh>
 #include <odreex/ppmpf/collections/sequence.hh>
 
-PPMPF_TEST( ppmpf_seq_at09_0
-          , "PPMPF_SEQ_AT09 - at index 0"
-          , PPMPF_SEQ_AT09(0,(a)(b)(c)(d)(e)(f)(g)(h)(i)(j))
-          , (a) )
+PPMPF_TST( ppmpf_seq_at09_0
+         , PPMPF_SEQ_AT09
+         , (0,(a)(b)(c)(d)(e)(f)(g)(h)(i)(j))
+         , (a)
+         , "PPMPF_SEQ_AT09 - at index 0" )
 
-PPMPF_TEST( ppmpf_seq_at09_1
-          , "PPMPF_SEQ_AT09 - at index 1"
-          , PPMPF_SEQ_AT09(1,(a)(b)(c)(d)(e)(f)(g)(h)(i)(j))
-          , (b) )
+PPMPF_TST( ppmpf_seq_at09_1
+         , PPMPF_SEQ_AT09
+         , (1,(a)(b)(c)(d)(e)(f)(g)(h)(i)(j))
+         , (b)
+         , "PPMPF_SEQ_AT09 - at index 1" )
 
-PPMPF_TEST( ppmpf_seq_at09_2
-          , "PPMPF_SEQ_AT09 - at index 2"
-          , PPMPF_SEQ_AT09(2,(a)(b)(c)(d)(e)(f)(g)(h)(i)(j))
-          , (c) )
+PPMPF_TST( ppmpf_seq_at09_2
+         , PPMPF_SEQ_AT09
+         , (2,(a)(b)(c)(d)(e)(f)(g)(h)(i)(j))
+         , (c)
+         , "PPMPF_SEQ_AT09 - at index 2" )
 
-PPMPF_TEST( ppmpf_seq_at09_3
-          , "PPMPF_SEQ_AT09 - at index 3"
-          , PPMPF_SEQ_AT09(3,(a)(b)(c)(d)(e)(f)(g)(h)(i)(j))
-          , (d) )
+PPMPF_TST( ppmpf_seq_at09_3
+         , PPMPF_SEQ_AT09
+         , (3,(a)(b)(c)(d)(e)(f)(g)(h)(i)(j))
+         , (d)
+         , "PPMPF_SEQ_AT09 - at index 3" )
 
-PPMPF_TEST( ppmpf_seq_at09_4
-          , "PPMPF_SEQ_AT09 - at index 4"
-          , PPMPF_SEQ_AT09(4,(a)(b)(c)(d)(e)(f)(g)(h)(i)(j))
-          , (e) )
+PPMPF_TST( ppmpf_seq_at09_4
+         , PPMPF_SEQ_AT09
+         , (4,(a)(b)(c)(d)(e)(f)(g)(h)(i)(j))
+         , (e)
+         , "PPMPF_SEQ_AT09 - at index 4" )
 
-PPMPF_TEST( ppmpf_seq_at09_5
-          , "PPMPF_SEQ_AT09 - at index 5"
-          , PPMPF_SEQ_AT09(5,(a)(b)(c)(d)(e)(f)(g)(h)(i)(j))
-          , (f) )
+PPMPF_TST( ppmpf_seq_at09_5
+         , PPMPF_SEQ_AT09
+         , (5,(a)(b)(c)(d)(e)(f)(g)(h)(i)(j))
+         , (f)
+         , "PPMPF_SEQ_AT09 - at index 5" )
 
-PPMPF_TEST( ppmpf_seq_at09_6
-          , "PPMPF_SEQ_AT09 - at index 6"
-          , PPMPF_SEQ_AT09(6,(a)(b)(c)(d)(e)(f)(g)(h)(i)(j))
-          , (g) )
+PPMPF_TST( ppmpf_seq_at09_6
+         , PPMPF_SEQ_AT09
+         , (6,(a)(b)(c)(d)(e)(f)(g)(h)(i)(j))
+         , (g)
+         , "PPMPF_SEQ_AT09 - at index 6" )
 
-PPMPF_TEST( ppmpf_seq_at09_7
-          , "PPMPF_SEQ_AT09 - at index 7"
-          , PPMPF_SEQ_AT09(7,(a)(b)(c)(d)(e)(f)(g)(h)(i)(j))
-          , (h) )
+PPMPF_TST( ppmpf_seq_at09_7
+         , PPMPF_SEQ_AT09
+         , (7,(a)(b)(c)(d)(e)(f)(g)(h)(i)(j))
+         , (h)
+         , "PPMPF_SEQ_AT09 - at index 7" )
 
-PPMPF_TEST( ppmpf_seq_at09_8
-          , "PPMPF_SEQ_AT09 - at index 8"
-          , PPMPF_SEQ_AT09(8,(a)(b)(c)(d)(e)(f)(g)(h)(i)(j) )
-          , (i) )
+PPMPF_TST( ppmpf_seq_at09_8
+         , PPMPF_SEQ_AT09
+         , (8,(a)(b)(c)(d)(e)(f)(g)(h)(i)(j))
+         , (i)
+         , "PPMPF_SEQ_AT09 - at index 8" )
 
-PPMPF_TEST( ppmpf_seq_at09_9
-          , "PPMPF_SEQ_AT09 - at index 9"
-          , PPMPF_SEQ_AT09(9,(a)(b)(c)(d)(e)(f)(g)(h)(i)(j) )
-          , (j) )
+PPMPF_TST( ppmpf_seq_at09_9
+         , PPMPF_SEQ_AT09
+         , (9,(a)(b)(c)(d)(e)(f)(g)(h)(i)(j))
+         , (j)
+         , "PPMPF_SEQ_AT09 - at index 9" )
 
 PPMPF_TEST_BLOCK( ppmpf
                 , check_seq_at09
@@ -82,7 +92,7 @@ PPMPF_TEST_BLOCK( ppmpf
                   , ppmpf_seq_at09_6
                   , ppmpf_seq_at09_7
                   , ppmpf_seq_at09_8
-                  , ppmpf_seq_at09_9)
+                  , ppmpf_seq_at09_9 )
                 , true
                 , "testing ppmpf PPMPF_SEQ_AT09 in index range [0,9]" )
 
--- tests/ppmpf/seq_zipwith.cc
+++ tests/ppmpf/seq_zipwith.cc
@@ -21,94 +21,102 @@
 #include <odreex/test.hh>
 #include <odreex/ppmpf/collections/sequence.hh>
 
-PPMPF_TEST( ppmpf_seq_zipwith
-          , "PPMPF_SEQ_ZIPWITH (zipwith using 2 ppmpf sequences)"
-          , PPMPF_SEQ_ZIPWITH( PPMPF_VCAT
-                             , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
-                             , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9) )
-          , (a0)(b1)(c2)(d3)(e4)(f5)(g6)(h7)(i8)(j9))
+PPMPF_TST( ppmpf_seq_zipwith
+         , PPMPF_SEQ_ZIPWITH
+         , ( PPMPF_VCAT
+           , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
+           , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9) )
+         , (a0)(b1)(c2)(d3)(e4)(f5)(g6)(h7)(i8)(j9)
+         , "PPMPF_SEQ_ZIPWITH (zipwith using 2 ppmpf sequences)" )
 
-PPMPF_TEST( ppmpf_seq_zipwith3
-          , "PPMPF_SEQ_ZIPWITH3 (zipwith using 3 ppmpf sequences)"
-          , PPMPF_SEQ_ZIPWITH3( PPMPF_VCAT
-                              , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
-                              , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9)
-                              , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j) )
-          , (a0a)(b1b)(c2c)(d3d)(e4e)(f5f)(g6g)(h7h)(i8i)(j9j))
+PPMPF_TST( ppmpf_seq_zipwith3
+         , PPMPF_SEQ_ZIPWITH3
+         , ( PPMPF_VCAT
+           , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
+           , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9)
+           , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j) )
+         , (a0a)(b1b)(c2c)(d3d)(e4e)(f5f)(g6g)(h7h)(i8i)(j9j)
+         , "PPMPF_SEQ_ZIPWITH3 (zipwith using 3 ppmpf sequences)" )
 
-PPMPF_TEST( ppmpf_seq_zipwith4
-          , "PPMPF_SEQ_ZIPWITH4 (zipwith using 4 ppmpf sequences)"
-          , PPMPF_SEQ_ZIPWITH4( PPMPF_VCAT
-                              , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
-                              , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9)
-                              , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
-                              , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9) )
-          , (a0a0)(b1b1)(c2c2)(d3d3)(e4e4)(f5f5)(g6g6)(h7h7)(i8i8)(j9j9))
+PPMPF_TST( ppmpf_seq_zipwith4
+         , PPMPF_SEQ_ZIPWITH4
+         , ( PPMPF_VCAT
+           , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
+           , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9)
+           , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
+           , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9) )
+         , (a0a0)(b1b1)(c2c2)(d3d3)(e4e4)(f5f5)(g6g6)(h7h7)(i8i8)(j9j9)
+         , "PPMPF_SEQ_ZIPWITH4 (zipwith using 4 ppmpf sequences)" )
 
-PPMPF_TEST( ppmpf_seq_zipwith5
-          , "PPMPF_SEQ_ZIPWITH5 (zipwith using 5 ppmpf sequences)"
-          , PPMPF_SEQ_ZIPWITH5( PPMPF_VCAT
-                              , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
-                              , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9)
-                              , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
-                              , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9)
-                              , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j) )
-          , (a0a0a)(b1b1b)(c2c2c)(d3d3d)(e4e4e)(f5f5f)(g6g6g)(h7h7h)(i8i8i)\
-(j9j9j))
+PPMPF_TST( ppmpf_seq_zipwith5
+         , PPMPF_SEQ_ZIPWITH5
+         , ( PPMPF_VCAT
+           , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
+           , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9)
+           , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
+           , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9)
+           , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j) )
+         , (a0a0a)(b1b1b)(c2c2c)(d3d3d)(e4e4e)(f5f5f)(g6g6g)(h7h7h)(i8i8i)\
+(j9j9j)
+         , "PPMPF_SEQ_ZIPWITH5 (zipwith using 5 ppmpf sequences)" )
 
-PPMPF_TEST( ppmpf_seq_zipwith6
-          , "PPMPF_SEQ_ZIPWITH6 (zipwith using 6 ppmpf sequences)"
-          , PPMPF_SEQ_ZIPWITH6( PPMPF_VCAT
-                              , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
-                              , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9)
-                              , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
-                              , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9)
-                              , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
-                              , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9) )
-          , (a0a0a0)(b1b1b1)(c2c2c2)(d3d3d3)(e4e4e4)(f5f5f5)(g6g6g6)(h7h7h7)\
-(i8i8i8)(j9j9j9))
+PPMPF_TST( ppmpf_seq_zipwith6
+         , PPMPF_SEQ_ZIPWITH6
+         , ( PPMPF_VCAT
+           , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
+           , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9)
+           , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
+           , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9)
+           , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
+           , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9) )
+         , (a0a0a0)(b1b1b1)(c2c2c2)(d3d3d3)(e4e4e4)(f5f5f5)(g6g6g6)(h7h7h7)\
+(i8i8i8)(j9j9j9)
+         , "PPMPF_SEQ_ZIPWITH6 (zipwith using 6 ppmpf sequences)" )
 
-PPMPF_TEST( ppmpf_seq_zipwith7
-          , "PPMPF_SEQ_ZIPWITH7 (zipwith using 7 ppmpf sequences)"
-          , PPMPF_SEQ_ZIPWITH7( PPMPF_VCAT
-                              , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
-                              , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9)
-                              , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
-                              , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9)
-                              , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
-                              , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9)
-                              , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j) )
-          , (a0a0a0a)(b1b1b1b)(c2c2c2c)(d3d3d3d)(e4e4e4e)(f5f5f5f)(g6g6g6g)\
-(h7h7h7h)(i8i8i8i)(j9j9j9j))
+PPMPF_TST( ppmpf_seq_zipwith7
+         , PPMPF_SEQ_ZIPWITH7
+         , ( PPMPF_VCAT
+           , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
+           , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9)
+           , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
+           , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9)
+           , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
+           , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9)
+           , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j) )
+         , (a0a0a0a)(b1b1b1b)(c2c2c2c)(d3d3d3d)(e4e4e4e)(f5f5f5f)(g6g6g6g)\
+(h7h7h7h)(i8i8i8i)(j9j9j9j)
+         , "PPMPF_SEQ_ZIPWITH7 (zipwith using 7 ppmpf sequences)" )
 
-PPMPF_TEST( ppmpf_seq_zipwith8
-          , "PPMPF_SEQ_ZIPWITH8 (zipwith using 8 ppmpf sequences)"
-          , PPMPF_SEQ_ZIPWITH8( PPMPF_VCAT
-                              , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
-                              , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9)
-                              , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
-                              , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9)
-                              , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
-                              , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9)
-                              , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
-                              , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9) )
-          , (a0a0a0a0)(b1b1b1b1)(c2c2c2c2)(d3d3d3d3)(e4e4e4e4)(f5f5f5f5)\
-(g6g6g6g6)(h7h7h7h7)(i8i8i8i8)(j9j9j9j9))
+PPMPF_TST( ppmpf_seq_zipwith8
+         , PPMPF_SEQ_ZIPWITH8
+         , ( PPMPF_VCAT
+           , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
+           , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9)
+           , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
+           , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9)
+           , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
+           , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9)
+           , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
+           , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9) )
+         , (a0a0a0a0)(b1b1b1b1)(c2c2c2c2)(d3d3d3d3)(e4e4e4e4)(f5f5f5f5)\
+(g6g6g6g6)(h7h7h7h7)(i8i8i8i8)(j9j9j9j9)
+         , "PPMPF_SEQ_ZIPWITH8 (zipwith using 8 ppmpf sequences)" )
 
-PPMPF_TEST( ppmpf_seq_zipwith9
-          , "PPMPF_SEQ_ZIPWITH9 (zipwith using 9 ppmpf sequences)"
-          , PPMPF_SEQ_ZIPWITH9( PPMPF_VCAT
-                              , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
-                              , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9)
-                              , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
-                              , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9)
-                              , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
-                              , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9)
-                              , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
-                              , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9)
-                              , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j) )
-          , (a0a0a0a0a)(b1b1b1b1b)(c2c2c2c2c)(d3d3d3d3d)(e4e4e4e4e)(f5f5f5f5f)\
-(g6g6g6g6g)(h7h7h7h7h)(i8i8i8i8i)(j9j9j9j9j))
+PPMPF_TST( ppmpf_seq_zipwith9
+         , PPMPF_SEQ_ZIPWITH9
+         , ( PPMPF_VCAT
+           , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
+           , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9)
+           , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
+           , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9)
+           , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
+           , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9)
+           , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)
+           , (0)(1)(2)(3)(4)(5)(6)(7)(8)(9)
+           , (a)(b)(c)(d)(e)(f)(g)(h)(i)(j) )
+         , (a0a0a0a0a)(b1b1b1b1b)(c2c2c2c2c)(d3d3d3d3d)(e4e4e4e4e)(f5f5f5f5f)\
+(g6g6g6g6g)(h7h7h7h7h)(i8i8i8i8i)(j9j9j9j9j)
+         , "PPMPF_SEQ_ZIPWITH9 (zipwith using 9 ppmpf sequences)" )
 
 PPMPF_TEST_BLOCK( ppmpf
                 , check_sequence_zipwith
