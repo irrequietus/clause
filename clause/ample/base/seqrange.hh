c2e7a3f64f5f1eabdcec940a0f24e8c85e5a7f06
George Makrydakis <george@irrequietus.eu>
2016-08-16 20:04:31 +0300

    Copyright notice missing on seqrange.hh fixed.

--- clause/ample/base/seqrange.hh
+++ clause/ample/base/seqrange.hh
@@ -1,8 +1,15 @@
-/*
- * intseq.hh
+/*~
+ * Copyright (C) 2013 - 2016 George Makrydakis <george@irrequietus.eu>
  *
- *  Created on: Aug 10, 2016
- *      Author: george
+ * This file is part of 'clause', a highly generic C++ meta-programming library,
+ * subject to the terms and conditions of the Mozilla Public License v 2.0. If
+ * a copy of the MPLv2 license text was not distributed with this file, you can
+ * obtain it at: http://mozilla.org/MPL/2.0/.
+ *
+ * The 'clause' library is an experimental library in active development with
+ * a source code repository at: https://github.com/irrequietus/clause.git and
+ * issue tracker at https://github.com/irrequietus/clause/issues.
+ *
  */
 
 #ifndef CLAUSE_AMPLE_BASE_SEQRANGE_HH
